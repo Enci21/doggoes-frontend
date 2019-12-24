769e0e131d22f51ab4745412211deaf01848603d
Enci21 <eni.hegedus.95@gmail.com>
2019-12-24 19:42:15 +0100

    Create LogIn form

---
+++ src/LogIn.js
@@ -0,0 +1,48 @@
+import React, {useState} from "react";
+import Axios from "axios";
+
+
+function LogIn() {
+
+    const [user, setUser] = useState({username: "", password: ""});
+
+    const showUserDetails = e => {
+        e.preventDefault();
+        Axios.post("http://localhost:8080/auth/signin", user)
+            .then(response => {
+                if (response.status) {
+                    alert("OK");
+                }
+            })
+            .catch(error => alert(error));
+    };
+
+    return (
+        <form className="reg-card" onSubmit={showUserDetails}>
+            <p className="input-names" style={{selfAlign: "center"}}>Log in</p>
+            <div>
+                <input
+                    style={{marginLeft: "10px", marginRight: "10px"}}
+                    required
+                    className="input"
+                    type="text"
+                    placeholder="Username"
+                    onChange={event => setUser({...user, username: event.target.value})}
+                />
+            </div>
+            <div>
+                <input
+                    style={{marginLeft: "10px", marginRight: "10px"}}
+                    required
+                    className="input"
+                    type="password"
+                    placeholder="Password"
+                    onChange={event => setUser({...user, password: event.target.value})}
+                />
+            </div>
+            <input type="submit" value="Log in" className="button" style={{marginBottom: "10%"}}/>
+        </form>
+    );
+}
+
+export default LogIn;
