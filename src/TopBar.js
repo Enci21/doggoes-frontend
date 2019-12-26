72f4eceed64a599ed18d4ef14bb4fd2ff3ddd781
Enci21 <eni.hegedus.95@gmail.com>
2019-12-26 18:54:25 +0100

    minor refactor

--- src/TopBar.js
+++ src/TopBar.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-
 function TopBar() {
 
     return (
-        <div>
-            <img src="logo1.png" className="logo" alt=""/>
+        <div className="top-bar">
+            <img src="logo1.png" className="logo"/>
         </div>
     );
+
 }
 
 export default TopBar;
