65869ce5ebbda7459dd10f91d7dcd98e8f76c037
Enci21 <eni.hegedus.95@gmail.com>
2020-04-12 18:45:51 +0200

    Redesign PlaceForm.js

--- src/PlaceForm.js
+++ src/PlaceForm.js
@@ -3,7 +3,18 @@ import Axios from "axios";
 
 function PlaceForm() {
 
-    const [place, setPlace] = useState({name: "", address: "", openingHours: "", number: ""});
+    const [place, setPlace] = useState({
+        name: "",
+        street: "",
+        building: "",
+        city: "",
+        state: "",
+        postalCode: "",
+        country: "",
+        openingHoursFrom: "",
+        openingHoursTo: "",
+        number: ""
+    });
     const sendPlaceData = e => {
         e.preventDefault();
         Axios.post("http://localhost:8080/place/new", place)
@@ -16,116 +27,133 @@ function PlaceForm() {
     };
 
     return (
-        <fieldset className="place-card">
-            <div>
-                <form onSubmit={sendPlaceData}>
-                    <input
-                        style={{marginTop: "5%"}}
-                        required
-                        name="name"
-                        id="placename"
-                        className="input"
-                        type="text"
-                        value={place.name}
-                        placeholder="Name"
-                        onChange={event => setPlace({...place, name: event.target.value})}
-                    />
+        <div>
+            <h1>Please recommend a new place dilom.</h1>
+            <fieldset className="place-card">
+                <div>
+                    <form onSubmit={sendPlaceData}>
+                        <input
+                            style={{marginTop: "5%"}}
+                            required
+                            name="name"
+                            id="placename"
+                            className="input"
+                            type="text"
+                            value={place.name}
+                            placeholder="Name"
+                            onChange={event => setPlace({...place, name: event.target.value})}
+                        />
 
-                    <input
-                        required
-                        name="city"
-                        id="city"
-                        className="input"
-                        type="text"
-                        value={place.city}
-                        placeholder="City"
-                        onChange={event => setPlace({...place, city: event.target.value})}
-                    />
+                        <input
+                            required
+                            name="street"
+                            id="street"
+                            className="input"
+                            type="text"
+                            value={place.street}
+                            placeholder="Street"
+                            onChange={event => setPlace({...place, street: event.target.value})}
+                        />
 
-                    <input
-                        required
-                        name="street"
-                        id="street"
-                        className="input"
-                        type="text"
-                        value={place.street}
-                        placeholder="Street"
-                        onChange={event => setPlace({...place, street: event.target.value})}
-                    />
+                        <input
+                            required
+                            name="building"
+                            id="building"
+                            className="input"
+                            type="text"
+                            value={place.building}
+                            placeholder="Building"
+                            onChange={event => setPlace({...place, building: event.target.value})}
+                        />
 
-                    <input
-                        required
-                        name="state"
-                        id="state"
-                        className="input"
-                        type="text"
-                        value={place.state}
-                        placeholder="State"
-                        onChange={event => setPlace({...place, state: event.target.value})}
-                    />
+                        <input
+                            required
+                            name="city"
+                            id="city"
+                            className="input"
+                            type="text"
+                            value={place.city}
+                            placeholder="City"
+                            onChange={event => setPlace({...place, city: event.target.value})}
+                        />
 
-                    <input
-                        required
-                        name="postal code"
-                        id="postal code"
-                        className="input"
-                        type="number"
-                        value={place.postalCode}
-                        placeholder="Postal code"
-                        onChange={event => setPlace({...place, postalCode: event.target.value})}
-                    />
 
-                    <input
-                        required
-                        name="country"
-                        id="country"
-                        className="input"
-                        type="text"
-                        value={place.country}
-                        placeholder="Country"
-                        onChange={event => setPlace({...place, country: event.target.value})}
-                    />
+                        <input
+                            style={{marginRight: "3%"}}
+                            required
+                            name="state"
+                            id="state"
+                            className="input"
+                            type="text"
+                            value={place.state}
+                            placeholder="State"
+                            onChange={event => setPlace({...place, state: event.target.value})}
+                        />
 
-                    <p style={{fontWeight: 'bold'}}> Opening hours</p>
-                    <input
-                        style={{marginRight: "2%"}}
-                        name="openingHoursFrom"
-                        id="openingHoursFrom"
-                        className="input"
-                        type="time"
-                        min="00:00"
-                        max="23:59"
-                        value={place.openingHoursFrom}
-                        onChange={event => setPlace({...place, openingHoursFrom: event.target.value})}
-                    />
+                        <input
+                            required
+                            name="postal code"
+                            id="postal code"
+                            className="input"
+                            type="number"
+                            min="1000"
+                            max="9999"
+                            value={place.postalCode}
+                            placeholder="Postal code"
+                            onChange={event => setPlace({...place, postalCode: event.target.value})}
+                        />
 
-                    <input
-                        name="openingHoursTo"
-                        id="openingHoursTo"
-                        className="input"
-                        type="time"
-                        min="00:00"
-                        max="23:59"
-                        value={place.openingHoursTo}
-                        onChange={event => setPlace({...place, openingHoursTo: event.target.value})}
-                    />
+                        <input
+                            required
+                            name="country"
+                            id="country"
+                            className="input"
+                            type="text"
+                            value={place.country}
+                            placeholder="Country"
+                            onChange={event => setPlace({...place, country: event.target.value})}
+                        />
 
-                    <input
-                        required
-                        name="phoneNumber"
-                        id="phoneNumber"
-                        className="input"
-                        type="text"
-                        value={place.number}
-                        placeholder="Phone number"
-                        onChange={event => setPlace({...place, number: event.target.value})}
-                    />
-                    <input type="submit" value="Recommend this place!" className="button"
-                           style={{marginBottom: "10%"}}/>
-                </form>
-            </div>
-        </fieldset>
+                        <p style={{fontWeight: 'bold'}}> Opening hours</p>
+                        <input
+                            style={{marginRight: "2%"}}
+                            name="openingHoursFrom"
+                            id="openingHoursFrom"
+                            className="input"
+                            type="time"
+                            min="00:00"
+                            max="23:59"
+                            value={place.openingHoursFrom}
+                            onChange={event => setPlace({...place, openingHoursFrom: event.target.value})}
+                        />
 
+                        <input
+                            name="openingHoursTo"
+                            id="openingHoursTo"
+                            className="input"
+                            type="time"
+                            min="00:00"
+                            max="23:59"
+                            value={place.openingHoursTo}
+                            onChange={event => setPlace({...place, openingHoursTo: event.target.value})}
+                        />
+
+                        <input
+
+                            name="phoneNumber"
+                            id="phoneNumber"
+                            className="input"
+                            type="text"
+                            value={place.number}
+                            placeholder="Phone number"
+                            onChange={event => setPlace({...place, number: event.target.value})}
+                        />
+                        <input type="submit" value="Recommend this place!" className="button"
+                               style={{marginBottom: "10%"}}/>
+                    </form>
+                </div>
+            </fieldset>
+        </div>
     );
 }
 
