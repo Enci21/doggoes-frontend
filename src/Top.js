916a05d2fcb025d81cedd5f9b1f163e5f3097eca
Enci21 <eni.hegedus.95@gmail.com>
2020-04-13 22:14:57 +0200

    Redesign the top

--- src/Top.js
+++ src/Top.js
@@ -1,6 +1,9 @@
 import logo from "./logo1.png";
 import React from "react";
 import Axios from "axios";
+import Tab from "@material-ui/core/Tab";
+import './App.css';
+import FavoriteIcon from "@material-ui/icons/Favorite";
 
 function Top() {
 
@@ -19,25 +22,35 @@ function Top() {
     return (
         <>
             <div className="top">
-                <h2>
-                    <i className={"fas fa-smile-beam icons"} style={{marginRight: "1%", float: "right"}}/>
-                    <a className="topButton"
-                       href="/auth/signin">{localStorage.getItem("username") != null ? localStorage.getItem("username") : "Log in"}</a>
-                </h2>
+                <img src={logo} className="logo"/>
+                <Tab label="Health" icon={<i className={"fas fa-medkit icons"}/>}/>
+                <Tab label="Friends" icon={<i className={"fas fa-paw icons"}/>}/>
+                <Tab label="Places" icon={<i className={"fas fa-map-marker-alt icons"}/>}/>
+                <Tab label="Sport" icon={<i className={"fas fa-baseball-ball icons"}/>}/>
+                <Tab label="Education" icon={<i className={"fas fa-graduation-cap icons"}/>}/>
+                <Tab label="Care" icon={<FavoriteIcon/>}/>
+                <Tab label="News" icon={<i className={"fas fa-newspaper icons"}/>}/>
+                <Tab label="Events" icon={<i className={"fas fa-calendar-alt icons"}/>}/>
+                <Tab label="Registration" icon={<i className={"fas fa-registered icons"}/>} href="/registration"/>
+                <Tab label={localStorage.getItem("username") != null ? localStorage.getItem("username") : "Log in"}
+                     icon={<i className={"fas fa-smile-beam icons"}/>} href="/auth/signin"/>
+                <Tab label="Log out"
+                     icon={<i className={"fas fa-times-circle icons"} href="/log/out" onClick={sendLogOut}/>}/>
 
 
-                <h2>
+                {/*  <h2>
                     <i className={"fas fa-registered icons"} style={{marginRight: "2%", float: "right"}}/>
                     <a className="topButton" href="/registration">Registration</a>
                 </h2>
-
                 <h2>
                     <i className={"fas fa-times-circle"} style={{marginRight: "2%", float: "right"}}/>
                     <a className="topButton" href="/log/out" onClick={sendLogOut}>Log out</a>
-                </h2>
-            </div>
-            <div className="topWithLogo">
-                <img src={logo} className="logo"/>
+                </h2>*/}
+                {/*<h2>
+                    <i className={"fas fa-smile-beam icons"} style={{marginRight: "1%", float: "right"}}/>
+                    <a className="topButton"
+                       href="/auth/signin">{localStorage.getItem("username") != null ? localStorage.getItem("username") : "Log in"}</a>
+                </h2>*/}
             </div>
         </>
     );
