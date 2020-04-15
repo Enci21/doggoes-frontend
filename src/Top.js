8cde53cc52a0e2605ded53fa2a042c73f4b5a72f
Enci21 <eni.hegedus.95@gmail.com>
2020-04-15 11:18:53 +0200

    Try to recreate scrollable tabs

--- src/Top.js
+++ src/Top.js
@@ -2,8 +2,10 @@ import logo from "./logo1.png";
 import React from "react";
 import Axios from "axios";
 import Tab from "@material-ui/core/Tab";
+import TabPanel from "@material-ui/core/Tab";
 import './App.css';
 import FavoriteIcon from "@material-ui/icons/Favorite";
+import PropTypes from "prop-types";
 
 function Top() {
 
@@ -19,37 +21,78 @@ function Top() {
             .catch(error => alert(error));
     };
 
+    const [value, setValue] = React.useState(0);
+
+    function a11yProps(index) {
+        return {
+            id: `scrollable-force-tab-${index}`,
+            'aria-controls': `scrollable-force-tabpanel-${index}`,
+        };
+    }
+
+    TabPanel.propTypes = {
+        children: PropTypes.node,
+        index: PropTypes.any.isRequired,
+        value: PropTypes.any.isRequired,
+    };
+
     return (
         <>
             <div className="top">
+
                 <img src={logo} className="logo"/>
-                <Tab label="Health" icon={<i className={"fas fa-medkit icons"}/>}/>
-                <Tab label="Friends" icon={<i className={"fas fa-paw icons"}/>}/>
-                <Tab label="Places" icon={<i className={"fas fa-map-marker-alt icons"}/>}/>
-                <Tab label="Sport" icon={<i className={"fas fa-baseball-ball icons"}/>}/>
+                <Tab label="Health" icon={<i className={"fas fa-medkit icons"} {...a11yProps(0)}/>}/>
+                <Tab label="Friends" icon={<i className={"fas fa-paw icons"}{...a11yProps(1)}/>}/>
+                <Tab label="Places" icon={<i className={"fas fa-map-marker-alt icons"} {...a11yProps(2)}/>}/>
+                <Tab label="Sport" icon={<i className={"fas fa-baseball-ball icons"}{...a11yProps(3)}/>}/>
                 <Tab label="Education" icon={<i className={"fas fa-graduation-cap icons"}/>}/>
                 <Tab label="Care" icon={<FavoriteIcon/>}/>
                 <Tab label="News" icon={<i className={"fas fa-newspaper icons"}/>}/>
                 <Tab label="Events" icon={<i className={"fas fa-calendar-alt icons"}/>}/>
-                <Tab label="Registration" icon={<i className={"fas fa-registered icons"}/>} href="/registration"/>
-                <Tab label={localStorage.getItem("username") != null ? localStorage.getItem("username") : "Log in"}
-                     icon={<i className={"fas fa-smile-beam icons"}/>} href="/auth/signin"/>
+                <Tab label="Registration" icon={<i className={"fas fa-registered icons"}/>}
+                     href="/registration"/>
+                <Tab
+                    label={localStorage.getItem("username") != null ? localStorage.getItem("username") : "Log in"}
+                    icon={<i className={"fas fa-smile-beam icons"}/>} href="/auth/signin"/>
                 <Tab label="Log out" icon={<i className={"fas fa-times-circle icons"} onClick={sendLogOut}/>}/>
 
+                <TabPanel value={value} index={0}>
+                    Vet <br/>
+                    Pharmacies <br/>
+                    Hospitals <br/>
+                    Psychologists <br/>
+                </TabPanel>
+                <TabPanel value={value} index={1}>
+                    Item Two
+                </TabPanel>
+                <TabPanel value={value} index={2}>
+                    Hotels <br/>
+                    Restaurants <br/>
+                    Pubs <br/>Geolocation
+                    Confectioneries <br/>
+                    Stores <br/>
+                </TabPanel>
+                <TabPanel value={value} index={3}>
+                    Dog running parks <br/>
+                    Tours <br/>
+                </TabPanel>
+                <TabPanel value={value} index={4}>
+                    Schools <br/>
+                    Trainers <br/>
+                    Day cares <br/>
+                </TabPanel>
+                <TabPanel value={value} index={5}>
+                    Food shops <br/>
+                    Equipment <br/>
+                    Walkers and babysitters <br/>
+                    Cosmetics <br/>
+                    Pet shops <br/>
+                    Masseures <br/>
+                </TabPanel>
+                <TabPanel value={value} index={6}>
+                    Breaking: A koronát nem kapják el a kutyák.
+                </TabPanel>
 
-                {/*  <h2>
-                    <i className={"fas fa-registered icons"} style={{marginRight: "2%", float: "right"}}/>
-                    <a className="topButton" href="/registration">Registration</a>
-                </h2>
-                <h2>
-                    <i className={"fas fa-times-circle"} style={{marginRight: "2%", float: "right"}}/>
-                    <a className="topButton" href="/log/out" onClick={sendLogOut}>Log out</a>
-                </h2>*/}
-                {/*<h2>
-                    <i className={"fas fa-smile-beam icons"} style={{marginRight: "1%", float: "right"}}/>
-                    <a className="topButton"
-                       href="/auth/signin">{localStorage.getItem("username") != null ? localStorage.getItem("username") : "Log in"}</a>
-                </h2>*/}
             </div>
         </>
     );
