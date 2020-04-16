5a76ebda72b7df80ada2021c1f0e92dc7ed81d03
Enci21 <eni.hegedus.95@gmail.com>
2020-04-16 15:22:52 +0200

    Add new Routes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Top from "./Top";
 import Geolocation from "./Geolocation";
 import PlaceForm from "./PlaceForm";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import HomePage from "./HomePage";
+import PlaceCard from "./PlaceCard";
+import WelcomePage from "./WelcomePage";
 
 function App() {
     const [uName, setUName] = useState({uname: localStorage.getItem("username" || "")});
@@ -17,9 +20,13 @@ function App() {
             <Router>
                 <div className="App">
                     <Switch>
+                        <Route path="/home" render={(props) => <HomePage {...props} />}/>
                         <Route path="/auth/signin" render={(props) => <LogIn {...props} setUname={setUName}/>}/>
                         <Route path="/registration" render={(props) => <RegistrationForm {...props} uName={uName}/>}/>
                         <Route path="/place/new" render={(props) => <PlaceForm {...props} />}/>
+                        <Route path="/place/all" render={(props) => <PlaceCard {...props} />}/>
+                        <Route path="/welcome" render={(props) => <WelcomePage {...props} />}/>
+
                     </Switch>
                 </div>
             </Router>
