de34d1573f13d1ceba655d021cd9641ee81f3c41
Enci21 <eni.hegedus.95@gmail.com>
2020-04-16 15:21:03 +0200

    Create HomePage

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,8 +1,24 @@
 import React from "react";
+import Axios from "axios";
 
 function homePage() {
 
+    const goHome = e => {
+        e.preventDefault();
+        Axios.get("http://localhost:8080/home")
+            .then(response => {
+                if (response.status) {
+                    alert("OK")
+                }
+            })
+            .catch(error => alert(error));
+    };
 
+    return (
+        <div style={{float: "left"}}>
+            <h1 style={{fontWeight: "bold"}}>Today most populars</h1>
+        </div>
+    )
 }
 
-export default homePage();
+export default homePage;
